adafc61bb484ee276667221bbff1822f202d804b
SauravNigam01 <sauravnigam9752@gmail.com>
2025-02-14 21:50:24 +0530

    Add files via upload

---
+++ ABC.py
@@ -0,0 +1,22 @@
+import google.generativeai as genai
+import streamlit as st  
+
+genai.configure(api_key="AIzaSyBfYBTLY9P0ubbiOMIqhtctYYd1kawWlXY")
+
+sys_prompt = """You are an advanced AI code reviewer with deep expertise in programming, best practices, and optimization techniques. Your task is to review the given code and provide the following:
+
+Correctness: Identify any logical, syntactical, or runtime errors. Suggest fixes if needed.
+Optimization: Suggest ways to improve performance, reduce time complexity, and enhance efficiency."""
+
+st.title("Code Review AI")
+userPrompt = st.text_area("Enter your Code:")
+
+
+gemini = genai.GenerativeModel(model_name="models/gemini-2.0-flash-exp", system_instruction=sys_prompt)
+
+if st.button("Generate Answer"):
+    if userPrompt.strip():  # Check if input is not empty
+        response = gemini.generate_content(userPrompt)
+        st.write(response.text)  # Ensure text output is displayed
+    else:
+        st.write("Please enter a valid Data Science query.")
